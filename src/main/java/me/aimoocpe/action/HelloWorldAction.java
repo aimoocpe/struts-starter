84f4eb34acaf4bf8f6343949741384259fbbb7e1
Theerawat Kaewchote <theerawatkc@gmail.com>
2017-06-30 01:41:22 +0700

    Using Struts2 Tags

--- src/main/java/me/aimoocpe/action/HelloWorldAction.java
+++ src/main/java/me/aimoocpe/action/HelloWorldAction.java
@@ -10,14 +10,19 @@ public class HelloWorldAction extends ActionSupport{
 	 */
 	private static final long serialVersionUID = 1L;
 	private MessageStore messageStore;
+	private static int helloCount = 0;
 
 	public String execute() {
 		messageStore = new MessageStore();
-
+		helloCount++;
 		return SUCCESS;
 	}
 
 	public MessageStore getMessageStore() {
 		return messageStore;
+	}
+	
+	public int getHelloCount() {
+	    return helloCount;
 	}
 }
